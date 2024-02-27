ba063d52acde9f780d92866058d2add1497501bb
clintyr <55336629+clintyr@users.noreply.github.com>
2024-02-27 13:56:25 -0500

    unit testing

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
-# import strategies
-from strategies import UnitBet, FixedPercentage, KellyCriterion
+from strategies import UnitBet, FixedPercentage, Martingale, AntiMartingale, KellyCriterion
+from strategies import WinnersBet, Fibonacci, AntiFibonacci, Dalembert, AntiDalembert
 
 bet_results = [["W", "W", "W", "Push", "Push", "W"],
                ["L", "L", "L", "Push", "L", "L"],
@@ -28,30 +28,13 @@ class UnitBetTest(unittest.TestCase):
 
         u_bet = UnitBet(bet_results, prob_info)
         u_bet.simulate_strategy()
-        print(u_bet.bankroll_values) 
+        # print(u_bet.bankroll_values) 
         self.assertEqual(expected_running_bankroll, u_bet.bankroll_values)
         self.assertEqual(expected_bet_sizes, u_bet.bet_sizes)
 
-# class FixedBetTest(unittest.TestCase):
-#     def test_bankroll_values(self):
-#         expected_bet_sizes = [[1]*len(bet_results[0])]*len(bet_results)
-#         expected_running_bankroll = [[101, 102, 103, 103, 103, 104],
-#                                      [99, 98, 97, 97, 96, 95],
-#                                      [99, 100, 99, 100, 99, 100],
-#                                      [101, 102, 103, 104, 105, 106],
-#                                      [99, 98, 97, 96, 95, 94],
-#                                      [100, 100, 100, 100, 100, 100],
-#                                      [101, 102, 103, 102, 101, 100]]
-
-#         f_bet = FixedBet(bet_results, prob_info, unit_size=1)
-#         f_bet.simulate_strategy()
-
-#         self.assertEqual(expected_running_bankroll, f_bet.bankroll_values)
-#         self.assertEqual(expected_bet_sizes, f_bet.bet_sizes)
-
 class FixedPercentageTest(unittest.TestCase):
     def test_bankroll_values(self):
-        expected_bet_sizes = [[1, 1.01, 1.02, 1.03, 1.03, 1.04],
+        expected_bet_sizes = [[1, 1.01, 1.02, 1.03, 1.03, 1.03],
                               [1, 0.99, 0.98, 0.97, 0.97, 0.96],
                               [1, 0.99, 1, 0.99, 1, 0.99],
                               [1, 1.01, 1.02, 1.03, 1.04, 1.05],
@@ -61,17 +44,63 @@ class FixedPercentageTest(unittest.TestCase):
         
         expected_running_bankroll = [[101, 102.01, 103.03, 103.03, 103.03, 104.06],
                                      [99, 98.01, 97.03, 97.03, 96.06, 95.1],
-                                     [99, 99.99, 98.99, 99.98, 98.98, 99.7],
-                                     [101, 102.01, 103.03, 104.06, 105.1, 106.05],
+                                     [99, 99.99, 98.99, 99.98, 98.98, 99.97],
+                                     [101, 102.01, 103.03, 104.06, 105.1, 106.15],
                                      [99, 98.01, 97.03, 96.06, 95.1, 94.15],
                                      [100, 100, 100, 100, 100, 100],
                                      [101, 102.01, 103.03, 102, 100.98, 99.97]]
 
         fp_bet = FixedPercentage(bet_results, prob_info)
         fp_bet.simulate_strategy()
-
+        # print(fp_bet.bankroll_values)
         self.assertEqual(expected_running_bankroll, fp_bet.bankroll_values)
         self.assertEqual(expected_bet_sizes, fp_bet.bet_sizes)
+
+class MartingaleTest(unittest.TestCase):
+    def test_bankroll_values(self):
+        expected_bet_sizes = [[1, 1, 1, 1, 1, 1],
+                              [1, 2, 4, 8, 8, 16],
+                              [1, 2, 1, 2, 1, 2],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 2, 4, 8, 16, 32],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 2, 4]]
+        
+        expected_running_bankroll = [[101, 102, 103, 103, 103, 104],
+                                     [99, 97, 93, 93, 85, 69],
+                                     [99, 101, 100, 102, 101, 103],
+                                     [101, 102, 103, 104, 105, 106],
+                                     [99, 97, 93, 85, 69, 37],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 102, 103, 102, 100, 96]]
+
+        m_bet = Martingale(bet_results, prob_info)
+        m_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, m_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, m_bet.bet_sizes)
+
+class AntiMartingaleTest(unittest.TestCase):
+    def test_bankroll_values(self):
+        expected_bet_sizes = [[1, 2, 4, 8, 8, 8],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 2, 1, 2, 1],
+                              [1, 2, 4, 8, 16, 32],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 2, 4, 8, 1, 1]]
+        
+        expected_running_bankroll = [[101, 103, 107, 107, 107, 115],
+                                     [99, 98, 97, 97, 96, 95],
+                                     [99, 100, 98, 99, 97, 98],
+                                     [101, 103, 107, 115, 131, 163],
+                                     [99, 98, 97, 96, 95, 94],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 103, 107, 99, 98, 97]]
+
+        am_bet = AntiMartingale(bet_results, prob_info)
+        am_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, am_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, am_bet.bet_sizes)
 
 class KellyCriterionTest(unittest.TestCase):
     def test_bankroll_values(self):
@@ -96,6 +125,114 @@ class KellyCriterionTest(unittest.TestCase):
 
         self.assertEqual(expected_running_bankroll, kc_bet.bankroll_values)
         self.assertEqual(expected_bet_sizes, kc_bet.bet_sizes)
+
+class WinnersBetTest(unittest.TestCase):
+    def test_bankroll_values(self):
+
+        expected_bet_sizes = [[1]*len(bet_results[0])]*len(bet_results)
+        
+        expected_running_bankroll = [[101, 102, 103, 103, 103, 104],
+                                     [99, 98, 97, 97, 96, 95],
+                                     [99, 100, 99, 100, 99, 100],
+                                     [101, 102, 103, 104, 105, 106],
+                                     [99, 98, 97, 96, 95, 94],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 102, 103, 102, 101, 100]]
+
+        w_bet = WinnersBet(bet_results, prob_info)
+        w_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, w_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, w_bet.bet_sizes)
+
+class FibonacciTest(unittest.TestCase):
+    def test_bankroll_values(self):
+        expected_bet_sizes = [[1, 1, 1, 1, 1, 1],
+                              [1, 1, 2, 2, 3, 5],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 2, 3, 5, 8],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 1, 2]]
+        
+        expected_running_bankroll = [[101, 102, 103, 103, 103, 104],
+                                     [99, 98, 96, 96, 93, 88],
+                                     [99, 100, 99, 100, 99, 100],
+                                     [101, 102, 103, 104, 105, 106],
+                                     [99, 98, 96, 93, 88, 80],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 102, 103, 102, 101, 99]]
+        f_bet = Fibonacci(bet_results, prob_info)
+        f_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, f_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, f_bet.bet_sizes)
+
+class AntiFibonacciTest(unittest.TestCase):
+    def test_bankroll_values(self):
+        expected_bet_sizes = [[1, 1, 2, 2, 2, 3],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 2, 3, 5, 8],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 2, 3, 1, 1]]
+        
+        expected_running_bankroll = [[101, 102, 104, 104, 104, 107],
+                                     [99, 98, 97, 97, 96, 95],
+                                     [99, 100, 99, 100, 99, 100],
+                                     [101, 102, 104, 107, 112, 120],
+                                     [99, 98, 97, 96, 95, 94],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 102, 104, 101, 100, 99]]
+        af_bet = AntiFibonacci(bet_results, prob_info)
+        af_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, af_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, af_bet.bet_sizes)
+
+class DalembertTest(unittest.TestCase):
+    def test_bankroll_values(self):
+        expected_bet_sizes = [[1, 2, 3, 4, 4, 4],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 2, 1, 2, 1],
+                              [1, 2, 3, 4, 5, 6],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 2, 3, 4, 3, 2]]
+        
+        expected_running_bankroll = [[101, 103, 106, 106, 106, 110],
+                                     [99, 98, 97, 97, 96, 95],
+                                     [99, 100, 98, 99, 97, 98],
+                                     [101, 103, 106, 110, 115, 121],
+                                     [99, 98, 97, 96, 95, 94],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 103, 106, 102, 99, 97]]
+        
+        d_bet = Dalembert(bet_results, prob_info)
+        d_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, d_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, d_bet.bet_sizes)
+
+class AntiDalembertTest(unittest.TestCase):
+    def test_bankroll_values(self):
+        expected_bet_sizes = [[1, 1, 1, 1, 1, 1],
+                              [1, 2, 3, 3, 4, 5],
+                              [1, 2, 1, 2, 1, 2],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 2, 3, 4, 5, 6],
+                              [1, 1, 1, 1, 1, 1],
+                              [1, 1, 1, 2, 3, 4]]
+        
+        expected_running_bankroll = [[101, 102, 103, 103, 103, 104],
+                                     [99, 97, 94, 94, 90, 85],
+                                     [99, 101, 100, 102, 101, 103],
+                                     [101, 102, 103, 104, 105, 106],
+                                     [99, 97, 94, 90, 85, 79],
+                                     [100, 100, 100, 100, 100, 100],
+                                     [101, 102, 103, 101, 98, 94]]
+        
+        ad_bet = AntiDalembert(bet_results, prob_info)
+        ad_bet.simulate_strategy()
+        self.assertEqual(expected_running_bankroll, ad_bet.bankroll_values)
+        self.assertEqual(expected_bet_sizes, ad_bet.bet_sizes)
 
 if __name__ == '__main__':
     unittest.main()
